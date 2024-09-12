e9532137d044015ca7e628f4bb897e579432a67d
Delusion-Zhong <zhong.ac61s@gmail.com>
2024-09-12 20:21:47 +0800

    1.1

--- STL/Vector/src/foreach.cpp
+++ STL/Vector/src/foreach.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 int main()
 {
-
+    // auto 自动推导数据类型
     vector<int> a = {1, 2, 3};
 
     for (auto i : a)
     {
         cout << i << endl;
     }
+
+    // 上面的一种foreach 用来你遍历
+    //  下面的这一种使用的是引用传递，然后给 i遍历的每一个值进行 *3 的 操作
     for (auto &i : a)
     {
         i *= 3;
