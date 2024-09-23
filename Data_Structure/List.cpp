c7c8fe13bd75b670bc419c478554b15572bbb0f1
Delusion-Zhong <zhong.ac61s@gmail.com>
2024-09-23 08:05:32 +0800

    main

--- Data_Structure/List.cpp
+++ Data_Structure/List.cpp
@@ -7,10 +7,15 @@ namespace List
     // 例如 存储 学生姓名,学号,成绩 单链表结构如下
     typedef struct student
     {
-        string  name;  //
+        string name; 
         long num;
         int score;
-    }ELemType;
+    } ELemType;
+    typedef struct Lnode
+    {
+        ELemType data;
+        struct Lnode *next;
+    } Lnode, *LinkList;
 
 }
 
