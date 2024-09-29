c71fd400fd91cc6ae6d9be58ce0c9393dd2ddf18
Delusion-Zhong <zhong.ac61s@gmail.com>
2024-09-29 16:41:36 +0800

    MAIN

--- Data_Structure/List.cpp
+++ Data_Structure/List.cpp
@@ -14,7 +14,8 @@ typedef int Status;
 typedef char ElemType;
 
 // 链表
-namespace LinkdList
+namespace LinkdList_demo
+
 {
     // 例如 存储 学生姓名,学号,成绩 单链表结构如下
     typedef struct student
@@ -33,18 +34,23 @@ namespace LinkdList
     } Lnode, *LinkList;
 
     // 运输符 重载
-    bool LinkdList::operator!=(const ElemType &th, const ElemType &data_b)
+    bool LinkdList_demo::operator!=(const ElemType &th, const ElemType &data_b)
     {
         return th.name != data_b.name || th.num != data_b.num || th.score != data_b.score;
     }
 
-    bool LinkdList::operator==(const ElemType &th, const ElemType &data_b)
+    bool LinkdList_demo::operator==(const ElemType &th, const ElemType &data_b)
     {
         return th.name == data_b.name && th.num == data_b.num && th.score == data_b.score;
     }
+    istream &LinkdList_demo::operator>>(istream &in, ElemType &data)
+    {
+        cin >> data.name >> data.num >> data.score; // 读取数据
+        return in;
+    }
 
     // 单链表的初始化 （带头节点的单链表）
-    Status LinkdList::LinkList_L(LinkList &L)
+    Status LinkdList_demo::LinkList_L(LinkList &L)
     {
         // 1.生成新的结点作为头节点，用头指针L 指向头节点
         // 2. 将头节点的指针域置为空
@@ -54,7 +60,7 @@ namespace LinkdList
     }
 
     // 判断连链表是否为空
-    Status LinkdList::ListEmpty(LinkList L)
+    Status LinkdList_demo::ListEmpty(LinkList L)
     {
 
         if (L->next)
@@ -68,7 +74,7 @@ namespace LinkdList
     }
 
     // 单链表的销毁，
-    Status LinkdList::DestroyLIst(LinkList &L)
+    Status LinkdList_demo::DestroyLIst(LinkList &L)
     {
         LinkList p;
         while (L)
@@ -82,7 +88,7 @@ namespace LinkdList
 
     // 清空单链表
     // 思路：依次释放所有节点，将头节点指针域设置为空
-    Status LinkdList::ReviseList(LinkList &L)
+    Status LinkdList_demo::ReviseList(LinkList &L)
     {
         LinkList P, Q;
         P = L->next;
@@ -99,7 +105,7 @@ namespace LinkdList
     }
 
     // 求单链表的表长
-    Status LinkdList::SizeList(LinkList L)
+    Status LinkdList_demo::SizeList(LinkList L)
     {
         // 先有一个指针，当指针下移 则++ （同时进行判断是否为空）
         LinkList p;
@@ -118,7 +124,7 @@ namespace LinkdList
     }
     /********************************************************************************************************* */
     // 查找元素(索引查找)
-    Status LinkdList::FindList(LinkList L, int index, ElemType &e)
+    Status LinkdList_demo::FindList(LinkList L, int index, ElemType &e)
     {
         LinkList p;
         int j = 1;
@@ -135,7 +141,7 @@ namespace LinkdList
     }
 
     // 按值查找（返回的是p指向linklist节点数据域)
-    LinkList LinkdList::Find_data_List(LinkList L, ElemType datp)
+    LinkList LinkdList_demo::Find_data_List(LinkList L, ElemType datp)
     {
         LinkList p;
         p = L->next;
@@ -143,8 +149,9 @@ namespace LinkdList
             p = p->next;
         return p;
     }
+
     // 按值查找（返回的位置的下标)
-    Status LinkdList::Find_index_List(LinkList L, ElemType datp)
+    Status LinkdList_demo::Find_index_List(LinkList L, ElemType datp)
     {
         LinkList p;
         p = L->next;
@@ -161,7 +168,7 @@ namespace LinkdList
     }
 
     // 插入
-    Status LinkdList::Insert_List(LinkList &L, ElemType datp, int index)
+    Status LinkdList_demo::Insert_List(LinkList &L, ElemType datp, int index)
     {
         LinkList p;
         int j = 0;
@@ -181,6 +188,63 @@ namespace LinkdList
         return OK;
     }
 
+    // 删除
+    Status LinkdList_demo::Delete_List(LinkList &L, int index) // 1  2  3  4   5
+    {
+        LinkList p, q; // 创建变量，存储前驱和后继
+        int j = 0;
+        p = L;
+        while (p->next && j < index - 1) // 删除节点的前驱
+        {                                // 判断p->next && j < index - 1  两个条件必须都要为真
+            p = p->next;
+            ++j;
+        }
+        if (!(p->next) || j > index - 1) // 判断 下一节点或者 j的下边是大于index的下标 则抛出异常
+            return ERROR;
+        q = p->next;             // 删除节点
+        p->next = q->next->next; // 将上一节点的指针 指向  下一节点的下一个节点
+        delete q;
+        return OK;
+    }
+
+    // 头插法
+    Status LinkdList_demo::Head_Insert_List(LinkList &L, int index)
+    {
+        LinkList Q = new Lnode; // 建立链表
+        Q->next = nullptr;      // 头节点的next为空
+        for (int i = index; i > 0; --i)
+        {
+            LinkList p = new Lnode;
+            cin >> p->data; // 当在这里在data中输入信息的时候，用到的（>> 运算符重载 ）
+            p->next = Q->next;
+            Q->next = p;
+        }
+        L = Q; // 将头节点返回给Q
+    }
+
+    // 尾差法     1    2       3      4         5   --->null
+    Status LinkdList_demo::Tail_Insert_List(LinkList &L, int index)
+    {
+        LinkList p = new Lnode; // 新节点
+        p->next = nullptr;
+        LinkList K = new Lnode; // 尾节点
+        while (L)
+        {
+            if (!(L->next))
+            {
+                K = L;
+            };
+            L = L->next;
+        }
+
+        for (int i = 0; i <= index; i++)
+        {
+            LinkList Q = new Lnode;
+            cin >> Q->data;
+            Q->next = nullptr;
+            K->next = Q;
+        }
+        }
 }
 
 int main()
